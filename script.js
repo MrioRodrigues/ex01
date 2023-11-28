09cee218cedf0e991b0bd1557cf4016806b163f9
MrioRodrigues <142705029+MrioRodrigues@users.noreply.github.com>
2023-11-27 21:00:18 -0300

    atualizacao2

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
+    var msgh = window.document.getElementById('msgh')
     var img = window.document.getElementById('imagem')
     var alt = window.document.getElementsByName('alt')
     var data = new Date()
     var hora = data.getHours()
     var min = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora} horas e ${min} min.`
+    msgh.innerHTML = `Agora são ${hora} horas e ${min} min.`
     if (hora >= 0 && hora < 12) {
+        msg.innerHTML = 'bom dia meu amor'
         img.src = 'imagens/manha.png'
         document.body.style.background = '#DEB894'
-    } else if (hora >= 12 && hora < 18) {   
+    } else if (hora >= 12 && hora < 18) { 
+        msg.innerHTML = 'boa tarde meu amor'  
         img.src = 'imagens/tarde.png'
         document.body.style.background = '#D58933'    
     } else {
+        msg.innerHTML = 'boa noite meu amor'
         img.src = 'imagens/noite.png'
         document.body.style.background = '#493652'
     }
